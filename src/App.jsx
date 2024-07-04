6e52cc4d5e6ee22902ec57663880d3832e1ca3b8
OmotolaJinadu <omotola.jinadu@fcmb.com>
2024-07-04 19:21:37 +0100

    created a profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
 import Button from "./components/Button";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import PictureOne from "./assets/images/portrait.jpg";
+import PictureTwo from "./assets/images/lady.jpg";
+import PictureThree from "./assets/images/guy.jpg";
 
 function App() {
+	const profileData = [
+		{
+			id: 1,
+			name: "Cynthia",
+			image: PictureOne,
+		},
+		{
+			id: 2,
+			name: "Beyonce",
+			image: PictureTwo,
+		},
+		{
+			id: 3,
+			name: "Damilare",
+			image: PictureThree,
+		},
+		{
+			id: 3,
+			name: "Damilare",
+			image: PictureThree,
+		},
+		{
+			id: 3,
+			name: "Damilare",
+			image: PictureThree,
+		},
+	];
+
 	return (
 		<div className="app">
 			<Navbar />
 			<div className="container">
-				<h1>Welcome to React</h1>
-				<Button />
+				{profileData.map((profile) => {
+					return (
+						<Profile name={profile.name} image={profile.image} />
+					);
+				})}
 			</div>
 		</div>
 	);
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ProfilePicture from "../assets/images/portrait.jpg";
+
+const Profile = (prop) => {
+	console.log(prop);
+	return (
+		<div className="profile">
+			<div className="info">
+				<h1>{prop.name}</h1>
+				<img src={prop.image} alt="profile-image" />
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
